8f4d939951623d2626cb3b3d3f551903505249ae
Bibash45 <bibashcdry46@gmail.com>
2025-01-03 13:14:08 +0545

    login and register components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+const App = lazy(() => import("./App"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
-
