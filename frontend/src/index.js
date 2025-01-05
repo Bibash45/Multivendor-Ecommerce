b96885981e38562d76b6195bd297266e2d4770c8
Bibash45 <bibashcdry46@gmail.com>
2025-01-05 13:25:03 +0545

    admin login comp. and RTK setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,30 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Toaster } from "react-hot-toast";
+import store from "./store/index";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 const App = lazy(() => import("./App"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
-    <Suspense fallback={<div>Loading...</div>}>
-      <App />
-    </Suspense>
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+        <Toaster
+          toastOptions={{
+            position: "top-center",
+            style: {
+              background: "#283046",
+              color: "#fff",
+            },
+          }}
+        />
+      </Suspense>
+    </Provider>
   </BrowserRouter>
   // </React.StrictMode>
 );
--- frontend/src/router/routes/publicRoutes.js
+++ frontend/src/router/routes/publicRoutes.js
@@ -1,7 +1,8 @@
 import { lazy } from "react";
 
-const Login = lazy(() => import("../../views/auth/Login"))
-const Register = lazy(() => import("../../views/auth/Register"))
+const AdminLogin = lazy(() => import("../../views/auth/AdminLogin"));
+const Login = lazy(() => import("../../views/auth/Login"));
+const Register = lazy(() => import("../../views/auth/Register"));
 
 const publicRoutes = [
   {
@@ -11,6 +12,10 @@ const publicRoutes = [
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/admin/login",
+    element: <AdminLogin />,
   },
 ];
 
