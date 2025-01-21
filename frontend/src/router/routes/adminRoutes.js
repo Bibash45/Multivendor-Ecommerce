5a59c7e6ebdbc42795fc602021013fbd05adae3c
Bibash45 <bibashcdry46@gmail.com>
2025-01-21 15:59:56 +0545

    seller page design

--- frontend/src/router/routes/adminRoutes.js
+++ frontend/src/router/routes/adminRoutes.js
@@ -3,6 +3,7 @@ import { lazy } from "react";
 const AdminDashboard = lazy(() => import("../../views/admin/AdminDashboard"));
 const Orders = lazy(() => import("../../views/admin/Orders"));
 const Category = lazy(() => import("../../views/admin/Category"));
+const Sellers = lazy(() => import("../../views/admin/Sellers"));
 
 export const adminRoutes = [
   {
@@ -19,5 +20,10 @@ export const adminRoutes = [
     path: "admin/dashboard/category",
     element: <Category />,
     role: "admin",
+  },
+  {
+    path: "admin/dashboard/sellers",
+    element: <Sellers />,
+    role: "seller",
   },
 ];
